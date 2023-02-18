05b3c3fa5cdd271e74b93c795ae10c6528f05e46
Shefat <you@example.com>
2023-02-18 08:55:29 +0600

    first commit

---
+++ project.js
@@ -0,0 +1,15 @@
+// this is 21 project .......
+
+
+// 21.2
+
+// i can convat ton to mon
+function tonToMon(ton){
+    let tonToKg = ton * 1000;
+    let KgToMon = tonToKg / 40;
+    return KgToMon;
+}
+
+let mon = tonToMon(4);
+console.log(mon);
+
